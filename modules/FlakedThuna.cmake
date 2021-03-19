7f0d17e1e3c64b3107bed9e6c0466bb704fcbc0e
flagarde <lagarde@sjtu.edu.cn>
2021-03-19 16:42:56 +0800

    Add FlakedThuna

---
+++ modules/FlakedThuna.cmake
@@ -0,0 +1,22 @@
+include_guard(GLOBAL)
+
+include(CPM)
+cpm()
+
+if(NOT DEFINED FLAKEDTUNA_VERSION)
+  set(FLAKEDTUNA_VERSION "1.0")
+endif()
+
+if(NOT DEFINED FLAKEDTUNA_REPOSITORY)
+  set(FLAKEDTUNA_REPOSITORY "https://gitlab.com/flagarde/FlakedTuna")
+endif()
+
+declare_option(REPOSITORY FlakedTuna OPTION BUILD_TESTS VALUE OFF)
+declare_option(REPOSITORY FlakedTuna OPTION BUILD_EXAMPLES VALUE OFF)
+print_options(REPOSITORY  FlakedTuna)
+
+CPMFindPackage(NAME FlakedTuna
+               GITHUB_REPOSITORY ${FLAKEDTUNA_REPOSITORY}
+               GIT_TAG ${FLAKEDTUNA_VERSION}
+               FETCHCONTENT_UPDATES_DISCONNECTED ${IS_OFFLINE}
+               OPTIONS ${FlakedTuna_OPTIONS})
