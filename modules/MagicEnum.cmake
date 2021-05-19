ef5559af3558274c6858b7f6d454d157d4cbc058
flagarde <8627746+flagarde@users.noreply.github.com>
2021-05-19 17:04:30 +0800

    Update MagicEnum.cmake

--- modules/MagicEnum.cmake
+++ modules/MagicEnum.cmake
@@ -7,8 +7,8 @@ if(NOT DEFINED MAGIC_ENUM_REPOSITORY)
   set(MAGIC_ENUM_REPOSITORY "https://github.com/Neargye/magic_enum.git")
 endif()
 
-if(NOT DEFINED MAGIC_ENUM_VERSION)
-  set(MAGIC_ENUM_VERSION "v0.7.2")
+if(NOT DEFINED MAGIC_ENUM_TAG)
+  set(MAGIC_ENUM_TAG "v0.7.2")
 endif()
 
 declare_option(REPOSITORY magic_enum OPTION MAGIC_ENUM_OPT_BUILD_EXAMPLES VALUE OFF)
@@ -18,6 +18,6 @@ print_options(REPOSITORY  magic_enum)
 
 CPMAddPackage(NAME magic_enum
               GIT_REPOSITORY ${MAGIC_ENUM_REPOSITORY}
-              GIT_TAG ${MAGIC_ENUM_VERSION}
+              GIT_TAG ${MAGIC_ENUM_TAG}
               FETCHCONTENT_UPDATES_DISCONNECTED ${IS_OFFLINE}
               OPTIONS "${magic_enum_OPTIONS}")
