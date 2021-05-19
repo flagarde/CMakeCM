2dd7215688b72c685379ec4d5e9696015a5a4ac2
flagarde <8627746+flagarde@users.noreply.github.com>
2021-05-19 17:07:36 +0800

    Update FlakedTuna.cmake

--- modules/FlakedTuna.cmake
+++ modules/FlakedTuna.cmake
@@ -3,12 +3,12 @@ include_guard(GLOBAL)
 include(CPM)
 cpm()
 
-if(NOT DEFINED FLAKEDTUNA_VERSION)
-  set(FLAKEDTUNA_VERSION "1.0")
+if(NOT DEFINED FLAKEDTUNA_TAG)
+  set(FLAKEDTUNA_TAG "v1.0")
 endif()
 
 if(NOT DEFINED FLAKEDTUNA_REPOSITORY)
-  set(FLAKEDTUNA_REPOSITORY "https://gitlab.com/flagarde/FlakedTuna.git")
+  set(FLAKEDTUNA_REPOSITORY "https://github.com/flagarde/FlakedTuna.git")
 endif()
 
 declare_option(REPOSITORY FlakedTuna OPTION BUILD_TESTS VALUE OFF)
@@ -17,6 +17,6 @@ print_options(REPOSITORY  FlakedTuna)
 
 CPMAddPackage(NAME FlakedTuna
                GIT_REPOSITORY ${FLAKEDTUNA_REPOSITORY}
-               VERSION ${FLAKEDTUNA_VERSION}
+               VERSION ${FLAKEDTUNA_TAG}
                FETCHCONTENT_UPDATES_DISCONNECTED ${IS_OFFLINE}
                OPTIONS ${FlakedTuna_OPTIONS})
