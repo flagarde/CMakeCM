4377c1215d9e0b517a803407ed42629285d83a20
flagarde <flagarde.programs@pm.me>
2021-05-26 05:06:03 +0000

    Update Jsoncpp.cmake

--- modules/Jsoncpp.cmake
+++ modules/Jsoncpp.cmake
@@ -47,4 +47,24 @@ if(COMPILE_JSONCPP)
                  GIT_TAG "${JSONCPP_TAG}"
                  FETCHCONTENT_UPDATES_DISCONNECTED ${IS_OFFLINE}
                  OPTIONS "${jsoncpp_OPTIONS}")
+                 
+  if(jsoncpp_ADDED)
+    get_option(VARIABLE "JSONCPP_SHARED" REPOSITORY "jsoncpp" OPTION "BUILD_SHARED_LIBS" )
+    get_option(VARIABLE "JSONCPP_STATIC" REPOSITORY "jsoncpp" OPTION "BUILD_STATIC_LIBS" )
+    get_option(VARIABLE "JSONCPP_OBJECT" REPOSITORY "jsoncpp" OPTION "BUILD_OBJECT_LIBS" )
+
+    add_library(json_internal INTERFACE)
+    if(${JSONCPP_SHARED} STREQUAL "ON")
+    	target_link_libraries(json_internal INTERFACE jsoncpp_lib)
+    endif()
+    if(${JSONCPP_STATIC} STREQUAL "ON")
+    	target_link_libraries(json_internal INTERFACE jsoncpp_static)
+    endif()
+    if(${JSONCPP_OBJECT} STREQUAL "ON")
+    	target_link_libraries(json_internal INTERFACE jsoncpp_object)
+    endif()
+
+    target_include_directories(json_internal INTERFACE $<BUILD_INTERFACE:${jsoncpp_SOURCE_DIR}/include> INTERFACE $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
+    add_library(jsoncpp::jsoncpp ALIAS json_internal)
+  endif()  
 endif()
