645414f3a5d4206c1f8be7c8b886822c9ff75f97
flagarde <flagarde.programs@pm.me>
2021-10-20 15:16:09 +0800

    Update Nlohmann.cmake

--- modules/Nlohmann.cmake
+++ modules/Nlohmann.cmake
@@ -8,7 +8,7 @@ if(NOT DEFINED NLOHMANN_REPOSITORY)
 endif()
 
 if(NOT DEFINED NLOHMANN_TAG)
-    set(NLOHMANN_TAG "v3.9.1")
+    set(NLOHMANN_TAG "v3.10.4")
 endif()
 
 declare_option(REPOSITORY nlohmann OPTION JSON_BuildTests VALUE OFF)
