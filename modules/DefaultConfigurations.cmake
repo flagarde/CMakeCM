238453513fb83658b4b08355a460de588c662bbf
flagarde <flagarde.programs@pm.me>
2021-12-02 22:22:20 +0800

    Try AllWarnings config

--- modules/DefaultConfigurations.cmake
+++ modules/DefaultConfigurations.cmake
@@ -59,3 +59,16 @@ add_configuration(CONFIG Coverage C_FLAGS "${C_COVERAGE_FLAG}" CXX_FLAGS "${CXX_
 unset(C_COVERAGE_FLAG)
 unset(CXX_COVERAGE_FLAG)
 unset(FORTRAN_COVERAGE_FLAG)
+
+# All warnings
+
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+  add_configuration(CONFIG AllWarnings
+            C_FLAGS "/W4 /w44060 /w44063 /w44063 /w44064 /w44065 /w44069 /w44137 /w44181 /w44203 /w44301 /w44303 /w44314 /w44318 /w44327 /w44328 /w44335 /w44370 /w44394 /w44413 /w44438 /w44448 /w44449 /w44450 /w44482 /w44495 /w44496 /w44497 /w44498 /w44509 /w44746 /w44755 /w44770 /w44771 /w44844 /w44854 /w44855 /w44856 /w44857 /w44900 /w44939 /w44954 /w44955 /w44970 /w44984 /w44986 /w44999 /w45022 /w45023 /w45034 /w45035 /w45059 /w45230"
+            CXX_FLAGS "/W4 /w44060 /w44063 /w44063 /w44064 /w44065 /w44069 /w44137 /w44181 /w44203 /w44301 /w44303 /w44314 /w44318 /w44327 /w44328 /w44335 /w44370 /w44394 /w44413 /w44438 /w44448 /w44449 /w44450 /w44482 /w44495 /w44496 /w44497 /w44498 /w44509 /w44746 /w44755 /w44770 /w44771 /w44844 /w44854 /w44855 /w44856 /w44857 /w44900 /w44939 /w44954 /w44955 /w44970 /w44984 /w44986 /w44999 /w45022 /w45023 /w45034 /w45035 /w45059 /w45230"
+          )
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  add_configuration(CONFIG AllWarnings C_FLAGS "-Wall -Wextra -Wpedantic" CXX_FLAGS "-Wall -Wextra -Wpedantic" Fortran_FLAGS "-Wall -Wextra -Wpedantic")
+elseif("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+  add_configuration(CONFIG AllWarnings C_FLAGS "-Wall -Weverything -Wpedantic" CXX_FLAGS "-Wall -Weverything -Wpedantic" Fortran_FLAGS "-Wall -Weverything -Wpedantic")
+endif()
