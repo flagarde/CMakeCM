c482eb7bb346a4fb903766963e88ae7911c125fd
flagarde <flagarde.programs@pm.me>
2021-12-03 09:44:41 +0800

    Update CPM.cmake

--- modules/CPM.cmake
+++ modules/CPM.cmake
@@ -23,6 +23,8 @@ endif()
 
 set(EMPTY_FILE "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
 
+set(CPM_USE_NAMED_CACHE_DIRECTORIES TRUE)
+
 macro(cpm)
   include(FetchContent)
   include(Ping)
