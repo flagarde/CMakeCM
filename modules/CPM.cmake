36f983a1132a160baf2011e5b8e276e390be01ab
flagarde <flagarde.programs@pm.me>
2022-01-03 17:29:47 +0800

    Update CPM.cmake

--- modules/CPM.cmake
+++ modules/CPM.cmake
@@ -9,7 +9,7 @@ endif()
 
 # If you don't provide version.
 if(NOT DEFINED CPM_DEFAULT_VERSION)
-  set(CPM_DEFAULT_VERSION "0.34.1")
+  set(CPM_DEFAULT_VERSION "0.34.3")
 endif()
 
 # Bypass the version given by the user.
