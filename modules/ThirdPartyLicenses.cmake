599713f5223991a66a548c949eba573b672f562c
flagarde <flagarde.programs@pm.me>
2022-01-14 20:39:58 +0800

    Update ThirdPartyLicenses.cmake

--- modules/ThirdPartyLicenses.cmake
+++ modules/ThirdPartyLicenses.cmake
@@ -19,6 +19,6 @@ CPMAddPackage(NAME CPMLicenses
 
 function(list_third_party_licenses FILENAME)
   cpm_licenses_create_disclaimer_target(write-license "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}" "${CPM_PACKAGES}")
-  add_custom_target(CPMLicenses ALL "" DEPENDS write-license BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}" COMMENT "Generating the third-party licenses list in ${FILENAME}")
+  add_custom_target(CPMLicenses ALL "" DEPENDS write-license BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}" COMMENT "Generating the third-party licenses list in ${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}")
   install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${FILENAME}" TYPE DOC)
 endfunction()
